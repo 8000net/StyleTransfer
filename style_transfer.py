41694dd8cfe6889a1d39e6b754e0a83f30cbe904
Justin Ledford <justinledford@gmail.com>
2017-10-15 10:48:28 -0500

    start fit_generator

--- style_transfer.py
+++ style_transfer.py
@@ -2,19 +2,27 @@ from keras.models import Model
 import keras.backend as K
 from keras.layers import Input
 from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator
 from scipy.misc import imresize
 import numpy as np
 
 from transform import TransformNet
 from loss import create_loss_fn
 
-# For testing:
-from loss import get_vgg_features, CONTENT_LAYER
-
-
 CONTENT_WEIGHT = 0.5
 STYLE_WEIGHT = 100
 TV_WEIGHT = 200
+
+def create_gen(img_dir, target_size, batch_size):
+    datagen = ImageDataGenerator()
+    gen = datagen.flow_from_directory(img_dir, target_size=target_size,
+                                      batch_size=batch_size, class_mode=None)
+
+    def tuple_gen():
+        for img in gen:
+            yield (img, img)
+
+    return tuple_gen
 
 style_img_path = 'wave.jpg'
 style_img = image.load_img(style_img_path)
@@ -27,7 +35,7 @@ content_img = image.load_img(content_img_path)
 content_img = imresize(content_img, (256, 256, 3))
 content_target = image.img_to_array(content_img)
 
-# Set 
+# Needed so that certain layers function in training mode (batch norm)
 K.set_learning_phase(1)
 
 inputs = Input(shape=(256, 256, 3))
@@ -36,7 +44,10 @@ model = Model(inputs=inputs, outputs=transform_net)
 loss_fn = create_loss_fn(style_target, CONTENT_WEIGHT, STYLE_WEIGHT, TV_WEIGHT)
 model.compile(optimizer='adam', loss=loss_fn)
 
-content_target = np.expand_dims(content_target, axis=0)
-X = [content_target]
-y = [content_target]
-model.fit(X, y)
+#content_target = np.expand_dims(content_target, axis=0)
+#X = [content_target]
+#y = [content_target]
+#model.fit(X, y)
+
+gen = create_gen('data', target_size=(256, 256), batch_size=1)
+model.fit_generator(gen, steps_per_epoch=82783)
