c7c24f06de0f46ff6404e4b01537a1a715c794f3
Justin Ledford <justinledford@gmail.com>
2017-10-16 10:42:13 -0500

    add preview callback

--- style_transfer.py
+++ style_transfer.py
@@ -1,9 +1,10 @@
 from keras.models import Model
 import keras.backend as K
+from keras.callbacks import Callback
 from keras.layers import Input
 from keras.preprocessing import image
 from keras.preprocessing.image import ImageDataGenerator
-from scipy.misc import imresize
+from scipy.misc import imresize, imsave
 import numpy as np
 import pandas as pd
 
@@ -27,16 +28,30 @@ def create_gen(img_dir, target_size, batch_size):
 
     return tuple_gen()
 
+
+class OutputPreview(Callback):
+    def __init__(self, test_img_path, increment):
+        test_img = image.load_img(test_img_path)
+        test_img = imresize(test_img, (256, 256, 3))
+        test_target = image.img_to_array(test_img)
+        test_target = np.expand_dims(test_target, axis=0)
+        self.test_img = test_target
+
+        self.increment = increment
+        self.batch_num = 0
+
+    def on_batch_end(self, batch, logs={}):
+        if (self.batch_num % self.increment == 0):
+            output = model.predict(self.test_img)[0]
+            imsave('preview/test-preview-%d.jpg' % self.batch_num, output)
+
+        self.batch_num += 1
+
+
+
 style_img_path = 'wave.jpg'
 style_img = image.load_img(style_img_path)
 style_target = image.img_to_array(style_img)
-
-content_img_path = 'content.jpg'
-content_img = image.load_img(content_img_path)
-
-# resize to 256x256 for training
-content_img = imresize(content_img, (256, 256, 3))
-content_target = image.img_to_array(content_img)
 
 # Needed so that certain layers function in training mode (batch norm)
 K.set_learning_phase(1)
@@ -54,6 +69,8 @@ model.compile(optimizer='adam', loss=loss_fn)
 #model.fit(X, y)
 
 gen = create_gen('data', target_size=(256, 256), batch_size=BATCH_SIZE)
-history = model.fit_generator(gen, steps_per_epoch=82780)
+output_preview = OutputPreview('doge.jpg', increment=50)
+history = model.fit_generator(gen, steps_per_epoch=82780,
+                              callbacks=[output_preview])
 model.save('wave-bs4.h5')
 pd.DataFrame(history.history).to_csv('wave-bs4.csv')
