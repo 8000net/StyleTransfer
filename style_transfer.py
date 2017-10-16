584521f87b43e55f21a502397b04313e921f5cf6
Justin Ledford <justinledford@gmail.com>
2017-10-16 11:20:47 -0500

    add debug function

--- style_transfer.py
+++ style_transfer.py
@@ -11,12 +11,6 @@ import pandas as pd
 from transform import TransformNet
 from loss import create_loss_fn
 
-CONTENT_WEIGHT = 0.5
-STYLE_WEIGHT = 100
-TV_WEIGHT = 200
-
-BATCH_SIZE = 4
-
 def create_gen(img_dir, target_size, batch_size):
     datagen = ImageDataGenerator()
     gen = datagen.flow_from_directory(img_dir, target_size=target_size,
@@ -29,48 +23,89 @@ def create_gen(img_dir, target_size, batch_size):
     return tuple_gen()
 
 
-class OutputPreview(Callback):
-    def __init__(self, test_img_path, increment):
-        test_img = image.load_img(test_img_path)
-        test_img = imresize(test_img, (256, 256, 3))
-        test_target = image.img_to_array(test_img)
-        test_target = np.expand_dims(test_target, axis=0)
-        self.test_img = test_target
+def debug(style_img_path, test_img_path, content_weight,
+        style_weight, tv_weight, n_gen, preview_increment):
 
-        self.increment = increment
-        self.batch_num = 0
+    def debug_gen(test_img_path, n):
+        content_img = image.load_img(test_img_path)
+        content_img = imresize(content_img, (256, 256, 3))
+        content_target = image.img_to_array(content_img)
+        content_target = np.expand_dims(content_target, axis=0)
 
-    def on_batch_end(self, batch, logs={}):
-        if (self.batch_num % self.increment == 0):
-            output = model.predict(self.test_img)[0]
-            imsave('preview/test-preview-%d.jpg' % self.batch_num, output)
-
-        self.batch_num += 1
+        for i in range(n):
+            yield (content_target, content_target)
+        return 
 
 
+    # This needs to be in scope where model is defined
+    class OutputPreview(Callback):
+        def __init__(self, test_img_path, increment):
+            test_img = image.load_img(test_img_path)
+            test_img = imresize(test_img, (256, 256, 3))
+            test_target = image.img_to_array(test_img)
+            test_target = np.expand_dims(test_target, axis=0)
+            self.test_img = test_target
 
-style_img_path = 'wave.jpg'
-style_img = image.load_img(style_img_path)
-style_target = image.img_to_array(style_img)
+            self.increment = increment
+            self.batch_num = 0
 
-# Needed so that certain layers function in training mode (batch norm)
-K.set_learning_phase(1)
+        def on_batch_end(self, batch, logs={}):
+            if (self.batch_num % self.increment == 0):
+                output = model.predict(self.test_img)[0]
+                imsave('preview/test-preview-%d.jpg' % self.batch_num, output)
 
-inputs = Input(shape=(256, 256, 3))
-transform_net = TransformNet(inputs)
-model = Model(inputs=inputs, outputs=transform_net)
-loss_fn = create_loss_fn(style_target, CONTENT_WEIGHT,
-                         STYLE_WEIGHT, TV_WEIGHT, BATCH_SIZE)
-model.compile(optimizer='adam', loss=loss_fn)
+            self.batch_num += 1
 
-#content_target = np.expand_dims(content_target, axis=0)
-#X = [content_target]
-#y = [content_target]
-#model.fit(X, y)
 
-gen = create_gen('data', target_size=(256, 256), batch_size=BATCH_SIZE)
-output_preview = OutputPreview('doge.jpg', increment=50)
-history = model.fit_generator(gen, steps_per_epoch=82780,
-                              callbacks=[output_preview])
-model.save('wave-bs4.h5')
-pd.DataFrame(history.history).to_csv('wave-bs4.csv')
+    style_img = image.load_img(style_img_path)
+    style_target = image.img_to_array(style_img)
+
+    inputs = Input(shape=(256, 256, 3))
+    transform_net = TransformNet(inputs)
+    model = Model(inputs=inputs, outputs=transform_net)
+    loss_fn = create_loss_fn(style_target, content_weight,
+                             style_weight, tv_weight, 1)
+    model.compile(optimizer='adam', loss=loss_fn)
+
+    gen = debug_gen(test_img_path, n_gen)
+    output_preview = OutputPreview(test_img_path, increment=preview_increment)
+    model.fit_generator(gen, steps_per_epoch=100,
+                        callbacks=[output_preview])
+
+
+def test(style_img_path, content_weight, style_weight, tv_weight, batch_size):
+    style_img = image.load_img(style_img_path)
+    style_target = image.img_to_array(style_img)
+
+    inputs = Input(shape=(256, 256, 3))
+    transform_net = TransformNet(inputs)
+    model = Model(inputs=inputs, outputs=transform_net)
+    loss_fn = create_loss_fn(style_target, content_weight,
+                             style_weight, tv_weight, batch_size)
+    model.compile(optimizer='adam', loss=loss_fn)
+
+    gen = create_gen('data', target_size=(256, 256), batch_size=BATCH_SIZE)
+    output_preview = OutputPreview('doge.jpg', increment=50)
+    history = model.fit_generator(gen, steps_per_epoch=82780,
+                                  callbacks=[output_preview])
+
+    model.save('wave-bs4.h5')
+    pd.DataFrame(history.history).to_csv('wave-bs4.csv')
+
+
+
+def main():
+    content_weight = 0.5
+    style_weight = 100
+    tv_weight = 200
+    batch_size = 4
+
+    # Needed so that certain layers function in training mode (batch norm)
+    K.set_learning_phase(1)
+
+    debug('wave.jpg', 'doge.jpg', content_weight,
+          style_weight, tv_weight, 100, 5)
+
+
+if __name__ == '__main__':
+    main()
