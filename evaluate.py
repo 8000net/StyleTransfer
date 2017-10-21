6c058db985ec435c7976552be90a2ceb7d52fde3
Justin Ledford <justinledford@gmail.com>
2017-10-21 17:20:29 -0500

    add reflection padding to evaluate

--- evaluate.py
+++ evaluate.py
@@ -15,12 +15,31 @@ def build_parser():
                         dest='output', help='output image path',
                         metavar='OUTPUT', required=True)
 
+    parser.add_argument('-p', '--pad',
+                        help='add reflection padding to input image',
+                        dest='pad', action='store_true')
+
+    parser.add_argument('-b', '--border-size', type=str,
+                        help='border size of reflection padding',
+                        dest='border_size', default=30)
+
     return parser
 
 
 def check_opts(options):
     assert os.path.exists(options.model), "model path not found!"
     assert os.path.exists(options.input), "input path not found!"
+
+
+def pad(img, border_size):
+    return np.pad(img, ((border_size, border_size),
+                        (border_size, border_size),
+                        (0,0)), mode='reflect')
+
+
+def unpad(img, border_size):
+    return img[border_size: -border_size, border_size: -border_size]
+
 
 parser = build_parser()
 options = parser.parse_args()
@@ -37,15 +56,19 @@ from transform import TransformNet
 # Get input image
 input_img = image.load_img(options.input)
 input_img = image.img_to_array(input_img)
-h, w, c = input_img.shape
+if options.pad:
+    input_img = pad(input_img, options.border_size)
 input_img = np.expand_dims(input_img, axis=0)
 
 
 # Load model
+_, h, w, c = input_img.shape
 inputs = Input(shape=(h, w, c))
 transform_net = TransformNet(inputs)
 model = Model(inputs, transform_net)
 model.load_weights(options.model)
 
-output = model.predict([input_img])[0]
-imsave(options.output, output)
+output_img = model.predict([input_img])[0]
+if options.pad:
+    output_img = unpad(output_img, options.border_size)
+imsave(options.output, output_img)
