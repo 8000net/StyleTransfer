7b9ff669e1339e629203dca91876ec2a11cff418
Justin Ledford <justinledford@gmail.com>
2017-10-21 16:58:02 -0500

    add argparse to evaluate script

--- evaluate.py
+++ evaluate.py
@@ -1,40 +1,51 @@
-from keras.models import load_model
+from argparse import ArgumentParser
+import os
+
+def build_parser():
+    parser = ArgumentParser()
+    parser.add_argument('--model', type=str,
+                        dest='model', help='model path',
+                        metavar='MODEL', required=True)
+
+    parser.add_argument('--input', type=str,
+                        dest='input', help='input image path',
+                        metavar='INPUT', required=True)
+
+    parser.add_argument('--output', type=str,
+                        dest='output', help='output image path',
+                        metavar='OUTPUT', required=True)
+
+    return parser
+
+
+def check_opts(options):
+    assert os.path.exists(options.model), "model path not found!"
+    assert os.path.exists(options.input), "input path not found!"
+
+parser = build_parser()
+options = parser.parse_args()
+check_opts(options)
+
+from keras.layers import Input
+from keras.models import Model
 from keras.preprocessing import image
 import numpy as np
-from scipy.misc import imresize, imsave
+from scipy.misc import imsave
 
-from keras_contrib.layers import InstanceNormalization
-from loss import create_loss_fn
+from transform import TransformNet
+
+# Get input image
+input_img = image.load_img(options.input)
+input_img = image.img_to_array(input_img)
+h, w, c = input_img.shape
+input_img = np.expand_dims(input_img, axis=0)
+
 
 # Load model
-CONTENT_WEIGHT = 0.5
-STYLE_WEIGHT = 100
-TV_WEIGHT = 200
+inputs = Input(shape=(h, w, c))
+transform_net = TransformNet(inputs)
+model = Model(inputs, transform_net)
+model.load_weights(options.model)
 
-BATCH_SIZE = 4
-
-
-style_img_path = 'wave.jpg'
-style_img = image.load_img(style_img_path)
-style_target = image.img_to_array(style_img)
-
-loss_fn = create_loss_fn(style_target, CONTENT_WEIGHT,
-                         STYLE_WEIGHT, TV_WEIGHT, BATCH_SIZE)
-
-model = load_model('wave-bs4.h5', custom_objects={
-    'InstanceNormalization': InstanceNormalization,
-    'style_transfer_loss': loss_fn
-})
-
-
-# Get output
-content_img_path = 'content.jpg'
-content_img = image.load_img(content_img_path)
-
-content_img = imresize(content_img, (256, 256, 3))
-content_target = image.img_to_array(content_img)
-content_target = np.expand_dims(content_target, axis=0)
-
-
-output = model.predict([content_target])[0]
-imsave('doge-bs4.jpg', output)
+output = model.predict([input_img])[0]
+imsave(options.output, output)
